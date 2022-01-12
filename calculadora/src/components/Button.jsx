2d30957f43ae44c3e0a6edfb05ec9d1f4226af18
Alberto Moura <palbertorm35@gmail.com>
2022-01-12 15:03:37 +0100

    12/01/22

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -4,7 +4,7 @@ import './Button.css';
 
 
 export default props  => {
-    let classes = 'button'
+    let classes = 'button  '
     classes += props.operation ? 'operation' : ''
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
